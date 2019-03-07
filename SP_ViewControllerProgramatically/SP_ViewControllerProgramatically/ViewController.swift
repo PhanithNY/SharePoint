d0ab701139b36d0c922b1308cef93b0b1b9b572f
Phanith NY <ny.phanith.fe@gmail.com>
2019-03-07 22:39:03 +0700

    • added first tutorial

---
+++ SP_ViewControllerProgramatically/SP_ViewControllerProgramatically/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SP_ViewControllerProgramatically
+//
+//  Created by PhanithNY on 3/6/19.
+//  Copyright © 2019 PhanithNY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.backgroundColor = UIColor.red
+  }
+
+
+}
+
